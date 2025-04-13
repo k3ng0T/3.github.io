794ff5ba25919d75fc4fe6044fcc8a222d6083f6
k3ng0T <131556304+k3ng0T@users.noreply.github.com>
2025-04-13 22:15:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.getElementById("submitBtn").addEventListener("click", function (e) {
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
+  
+    let valid = true
+
+  
+  
+    if (!email.value.trim().match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      alert("Пожалуста, напишите еmail")
+      valid = false
+    }
+  
+    if (password.value.length < 8) {
+      alert("Пароль должен быть не менее 8 символов")
+      valid = false
+    }
+  
+
+    document.querySelector("form").addEventListener("submit", function(e) {
+        e.preventDefault()
+      
+        const button = document.getElementById("submitBtn")
+        const formWrapper = document.getElementById("formWrapper")
+      
+
+        if (valid){
+            formWrapper.classList.add("hidden")
+            button.classList.add("animating")
+            setTimeout(() => {
+                window.location.href = "index.html"
+              }, 1500)
+        }else{
+            setTimeout(() => {
+                window.location.href = "index.html"
+              }, 1)
+        }
+      })
+
+
+
+
+  })
+  
+
+
+  
